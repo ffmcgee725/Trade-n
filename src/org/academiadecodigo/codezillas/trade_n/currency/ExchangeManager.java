27c15f911561be069de2f9c32b0a110049f2c2da
Emanuel Barcelos <ebarcelos@gmail.com>
2019-07-13 16:06:28 +0000

    Included API connection for currency exchange

--- src/org/academiadecodigo/codezillas/trade_n/currency/ExchangeManager.java
+++ src/org/academiadecodigo/codezillas/trade_n/currency/ExchangeManager.java
@@ -1,4 +1,38 @@
 package org.academiadecodigo.codezillas.trade_n.currency;
 
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class ExchangeManager {
+
+    public double getRates(CurrencyType base, CurrencyType target) {
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://api.cryptonator.com/api/ticker/" + base.getCode() + "-" + target.getCode()))
+                .header("Accept", "application/json")
+                .build();
+
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            JSONObject json = new JSONObject(response.body());
+            JSONObject ticker = json.getJSONObject("ticker");
+            double rate = ticker.getDouble("price");
+
+            return rate;
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return 0;
+    }
 }
