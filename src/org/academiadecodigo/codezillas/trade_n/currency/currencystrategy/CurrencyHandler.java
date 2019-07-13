c50d9e23f9463391ffb35b40c7d926d23bc66e30
Reinaldo Pimentel <reinaldo.pimentel@ymail.com>
2019-07-13 13:44:01 +0000

    Added prompt menu for currency selection with strategy pattern

--- src/org/academiadecodigo/codezillas/trade_n/currency/currencystrategy/CurrencyHandler.java
+++ src/org/academiadecodigo/codezillas/trade_n/currency/currencystrategy/CurrencyHandler.java
@@ -2,9 +2,7 @@ package org.academiadecodigo.codezillas.trade_n.currency.currencystrategy;
 
 import org.academiadecodigo.codezillas.trade_n.server.ClientHandler;
 
-public class BitcoinStrategy implements CurrencyHandler{
-    @Override
-    public void execute(ClientHandler clientHandler) {
+public interface CurrencyHandler {
 
-    }
+    public void execute(ClientHandler clientHandler);
 }
